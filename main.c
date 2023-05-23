4c5caba1c8473a0500126d71035437d57cf50f09
mucahit <rwx@overflow.localdomain>
2023-05-24 00:45:46 +0300

    added structre and setup function
    Signed-off-by: mucahit <rwx@overflow.localdomain>

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 #include "utils.h"
+
+struct {
+	int fd;	
+} *ed;
+
+void setup() {
+	ed = mmap(NULL, 4096 * 2, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (ed == MAP_FAILED) die("inital map of setup");
+}
 
 int main() {
 	in();
