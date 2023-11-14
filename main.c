4995980c4a82d7e9def5dda7b4f9023486cac3d2
Adınız <siz@e-posta.com>
2023-11-14 17:53:11 +0300

    banner is added
    Signed-off-by: Adınız <siz@e-posta.com>

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <fcntl.h>
 
 #include "utils.h"
 
@@ -10,8 +11,17 @@ struct {
 } *ed;
 
 void setup() {
+	note("\t\t###################################################");
+	note("\t\t# 盲目人間はこの世界で色がありませんに思うか？    #");
+	note("\t\t# この人は思うと道がありません。人間は思うるです。#");
+	note("\t\t###################################################");
+	note("\t\t				    Zedeleyici.1337");
 	ed = mmap(NULL, 4096 * 2, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (ed == MAP_FAILED) die("inital map of setup");
+
+	ed->fd = open("/dev/dri/card0", O_RDWR);
+	if (ed->fd < 0) die("open Failed");
+
 }
 
 int main() {
