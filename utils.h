1c3512db35e8c2db39848ba135c75575ad71d39e
Adınız <siz@e-posta.com>
2024-09-01 16:14:40 +0300

    fixing utils
    Signed-off-by: Adınız <siz@e-posta.com>

--- utils.h
+++ utils.h
@@ -13,11 +13,11 @@ void print_progress_bar(size_t progress, size_t total);
 #define pd(val) note("int(%s) : %d", #val, val)
 #define pu(val) note("unsigned int(%s) : %u", #val, val)
 #define px(val) note("int hex(%s) : %x", #val, val)
-#define pf(val) note("int hex(%s) : %f", #val, val)
+#define pf(val) note("float(%s) : %f", #val, val)
 #define plu(val) note("unsigned long(%s) : %lu", #val, val)
 #define pld(val) note("long(%s) : %ld", #val, val)
 #define plx(val) note("long hex(%s) : %lx", #val, val)
-#define plf(val) note("long hex(%s) : %lf", #val, val)
+#define plf(val) note("double(%s) : %lf", #val, val)
 
 
 #define in() {note("Calling => %s",__func__);}
